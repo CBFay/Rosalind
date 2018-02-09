53cdd756ebb4c8dee44f8154d604e1adbd655029
Craig-Benjamin <31521775+CBFay@users.noreply.github.com>
2018-02-08 22:41:56 -0500

    Create rosalind_0010.py

---
+++ Rosalind 0010 - Consensus and Profile/rosalind_0010.py
@@ -0,0 +1,28 @@
+# Format input
+with open('rosalind_cons2.txt') as file:
+    dna = []
+    labels = []
+    
+    for line in file.read().splitlines():
+        if line[0] == '>':
+            labels.append(line)
+            dna.append('')
+        else:
+            dna[-1] += line
+
+# Populate a profile matrix
+size = len(dna[0]) # helper variable
+profile = dict(zip((b for b in 'ACGT'), ([0] * size for i in range(4))))
+
+for sample in dna:
+    for i in range(size):
+        profile[sample[i]][i] += 1
+    
+# Build a concensus list
+consensus = "".join([max(profile, key=(lambda k: profile[k][i])) \
+             for i in range(size)])
+    
+# Format output
+print(consensus)
+for base in profile:
+    print(base + ':', " ".join(str(n) for n in profile[base]))
